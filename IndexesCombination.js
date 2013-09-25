1f97b501cf0355e2dd784d6195ca6a2ed1d84003
forchr <foundwai@gmail.com>
2013-09-25 15:45:39 +0800

    Update IndexesCombination.js

--- IndexesCombination.js
+++ IndexesCombination.js
@@ -2,57 +2,67 @@
  * 计算下标组合。
  * @param {Array of int} maxes index maximums.
  * @param {Array of int} curs current indexes.
+ * @param {Array of int} mins index minimums.
  */
-function IndexesCombination(maxes, curs) {
+function IndexesCombination(maxes, curs, mins) {
     if (!(maxes instanceof Array)) {
         throw "must supply maxes array.";
     }
+
+    this.getSize = function() {
+        return maxes.length;
+    };
+
+    var last = maxes.length - 1;
+    if (!(mins instanceof Array)) {
+        mins = new Array();
+    }
+    if (!(curs instanceof Array)) {
+        curs = new Array();
+    }
+    
     var total = 1;
     for (var i = 0; i < maxes.length; i++) {
         if (maxes[i] < 0) {
             throw "maximum must be greater than zero.";
         }
-        total *= maxes[i] + 1;
-    }
-    this.getMaxes = function() {
-        var a = [];
-        for (var i = 0; i < maxes.length; i++) {
-            a[i] = maxes[i];
+
+        if (typeof mins[i] !== "number" || mins[i] < 0) {
+            mins[i] = 0;
+        } else if (mins[i] > maxes[i]) {
+            var t = mins[i];
+            mins[i] = maxes[i];
+            maxes[i] = t;
         }
-        return a;
-    };
+        if (typeof curs[i] !== "number" || curs[i] < mins[i]) {
+            curs[i] = i === last ? mins[i] - 1 : mins[i];
+        } else if (curs[i] > maxes[i]) {
+            curs[i] = maxes[i];
+        }
+        
+        total *= maxes[i] - mins[i] + 1;
+    }
+
+    /**
+     * 
+     * @returns {Number} 剩下多少个
+     */
     this.getTotal = function() {
         return total;
     };
-    this.getSize = function() {
-        return maxes.length;
-    };
-    var last = maxes.length - 1;
-    if (!(curs instanceof Array)) {
-        curs = new Array();
-    }
-    for (var i = 0; i < last; i++) {
-        if (typeof curs[i] !== "number" || curs[i] < 0) {
-            curs[i] = 0;
-        }
-    }
-
-    if (typeof curs[last] !== "number" || curs[last] < -1) {
-        curs[last] = -1;
-    }
 
     this.reset = function() {
-        for (var i = 0; i < last; i++) {
-            curs[i] = 0;
+        for (var i = 0; i < maxes.length; i++) {
+            curs[i] = mins[i];
         }
-        curs[last] = -1;
     };
+
     this.next = function(store) {
         if (!(store instanceof Array)) {
             throw "must supply a array to store indexes."
         }
         curs[last]++;
-        var aoi = maxes.length;
+        var aoi = maxes.length;//increment one element index
         for (var i = last; i >= 0; i--) {
             if (curs[i] > maxes[i]) {
                 aoi = i - 1;
@@ -69,7 +79,7 @@ function IndexesCombination(maxes, curs) {
             return false;
         }
         for (var i = aoi + 1; i < maxes.length; i++) {
-            curs[i] = 0;
+            curs[i] = mins[i];
         }
         copyIndexes(store);
         return true;
@@ -90,15 +100,22 @@ function IndexesCombination(maxes, curs) {
         if (!(a instanceof Array)) {
             throw "param must be a array";
         }
-        for (var i = 0; i < maxes.length - 1; i++) {
-            curs[i] = a[i];
-            if (curs[i] <= -1) {
-                curs[i] = 0;
+        for (var i = 0; i < maxes.length; i++) {
+            if (a[i] >= mins[i] && a[i] <= maxes[i]) {
+                curs[i] = a[i];
             }
         }
-        curs[i] = a[i];
-        if (a[i] <= -1) {
-            curs[i] = -1;
+    };
+
+    this.setIndexesAt = function(cur, min, max, at) {
+        if (at >= 0
+                && at < maxes.length
+                && min >= defMins[at]
+                && max >= 0
+                && min <= max
+                && cur >= min
+                && cur <= max) {
+            curs[at] = cur;
         }
     };
 }
